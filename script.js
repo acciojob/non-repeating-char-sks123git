201a6d373a80cbd749845ec36d0ac2e58509460d
Sks <124144700+sks123git@users.noreply.github.com>
2026-06-17 19:23:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function firstNonRepeatedChar(str) {
-  let charMap = new Map()
-	for (let index = 0; index < str.length; index++) {
-		charMap[str[index]] = (charMap[str[index]] || 0) + 1
-	}
-	for(const [key, val] of Object.entries(charMap)) {
-	    if(val === 1) { 
-	       return key
-	        break
-	    }
-	}
+   let charMap = {};
+
+  for (let i = 0; i < str.length; i++) {
+    charMap[str[i]] = (charMap[str[i]] || 0) + 1;
+  }
+
+  for (const key in charMap) {
+    if (charMap[key] === 1) {
+      return key;
+    }
+  }
+
+  return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
